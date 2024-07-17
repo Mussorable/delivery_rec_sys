5355d4887023b02f1fd2be4b1f170c60c621bcaf
Mussorable <redduck5601@gmail.com>
2024-07-17 21:12:13 +0200

    make init .db login logout handling

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,17 @@
-from flask import flash, redirect, render_template
+from urllib.parse import urlsplit
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required, login_user, logout_user
 
-from app import app
+import sqlalchemy as sa
+
+from app import app, db
 from app.forms import LoginForm
+from app.models import User
 
 @app.route('/')
 
 @app.route('/index')
+@login_required
 def index():
     user = {
         'username': 'Gajawecki',
@@ -16,11 +22,30 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     
+    form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
         
-        return redirect('/index')
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            
+            return redirect(url_for('login'))
+        
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    
+    return redirect(url_for('index'))
